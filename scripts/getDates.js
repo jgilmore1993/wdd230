a13c14906789b3340dd4059ef08099f5f77dc1f0
Jordan Gilmore Account <jordan.c.gilmore@gmail.com>
2024-01-22 21:43:42 -0500

    Commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -15,3 +15,17 @@ const lastModifiedParagraph = document.getElementById('lastModified');
 lastModifiedParagraph.textContent = 'Last modified: ' + lastModified.toLocaleString();
 
 document.querySelector("p").textContent = document.lastModified;
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
