5a3886e9e4997d52a83bb440081b41a2c3b8b63d
Jordan Gilmore Account <jordan.c.gilmore@gmail.com>
2024-03-05 11:19:47 -0500

    Commit

---
+++ scripts/links.js
@@ -0,0 +1,21 @@
+const baseURL = "https://yourgithubusername.github.io/wdd230/";
+
+const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  getLinks();
+
+  async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+  function displayLinks(weeks) {
+    // Code to display links based on the weeks array will go here
+  }
+  
