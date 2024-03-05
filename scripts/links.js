6b041e5aaccdd8f3d0e4c55aeb1dfb2cd2f3a3e5
Jordan Gilmore Account <jordan.c.gilmore@gmail.com>
2024-03-05 11:54:37 -0500

    Commit

--- scripts/links.js
+++ scripts/links.js
@@ -3,19 +3,28 @@ const baseURL = "https://github.com/jgilmore1993/wdd230";
 const linksURL = "https://raw.githubusercontent.com/jgilmore1993/wdd230/main/data/links.json";
 
 async function getLinks() {
+    
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data)
+
   }
   
   getLinks();
 
-  async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
-  }
+
   function displayLinks(weeks) {
-    // Code to display links based on the weeks array will go here
+    let ul = document.getElementById("lessons")
+
+    weeks.lessons.forEach(lesson => {
+        let li = document.createElement("li")
+        let a = document.createElement("a")
+        li.innerHTML = lesson.lesson + ": "
+        a.setAttribute("href", lesson.links[0].url)
+        a.innerHTML = lesson.links[0].title
+        li.appendChild(a)
+        ul.appendChild(li)
+    });
+    
   }
   
