ffe64dd7f1681bb0ad6d64dc63d5cc7ed493e3c5
Jordan Gilmore Account <jordan.c.gilmore@gmail.com>
2024-03-11 16:03:15 -0400

    Commit

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,30 @@
+lat = 43.4666
+lon = -112.0341
 const apiKey = 'ad62857b9636992224b8ca8bd61b0725'; // Replace YOUR_API_KEY with your actual API key
 const city = 'Rochester, NY';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-fetch(url)
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+async function getweatherData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    displayweather(data)
+  }
+
+getweatherData(url)
+
+function displayweather(data){
+        let temperature = document.getElementById("temperature")
+        let icon = document.getElementById("icon")
+        let condition = document.getElementById("condition")
+
+        temperature.innerHTML = data.main.temp
+    
+        icon.setAttribute('src', weather.imageurl);
+        icon.setAttribute('alt', `Portrait of ${weather.name} ${weather.lastname}`); // fill in the blank
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '340');
+        icon.setAttribute('height', '440');
+        console.log(data.weather)[0][0]
+        condition.innerHTML = data.weather[0].desrciption
+}
